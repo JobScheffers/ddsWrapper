5c803b469e2e5df07471fd641d5652b6b9594732
Job Scheffers <jobscheffers@hotmail.com>
2024-10-16 14:20:13 +0200

    dds2

--- ddsWrapper/ddsWrapper.cs
+++ ddsWrapper/ddsWrapper.cs
@@ -140,16 +140,16 @@ namespace DDS
 
         #region Imports
 
-        [DllImport("dds", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("dds2", CallingConvention = CallingConvention.Cdecl)]
         private static extern int CalcDDtablePBN(ddTableDealPBN tableDealPBN, ref ddTableResults tablep);
 
-        [DllImport("dds", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("dds2", CallingConvention = CallingConvention.Cdecl)]
         private static extern int CalcAllTablesPBN(ddTableDealsPBN deals, int mode, int[] trumpFilter, ref ddTablesResult tableResults, ref allParResults parResults);
 
-        [DllImport("dds", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("dds2", CallingConvention = CallingConvention.Cdecl)]
         private static extern int CalcAllTables(ddTableDeals deals, int mode, int[] trumpFilter, ref ddTablesResult tableResults, ref allParResults parResults);
 
-        [DllImport("dds", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("dds2", CallingConvention = CallingConvention.Cdecl)]
         private static extern int DealerPar(ref ddTableResults tablep, ref parResultsDealer presp, int dealer, int vulnerable);
 
         #endregion
@@ -343,7 +343,7 @@ namespace DDS
 
         static ddsWrapper()
         {
-            ResourceExtractor.ExtractResourceToFile("ddsWrapper.dds.dll", "dds.dll");
+            ResourceExtractor.ExtractResourceToFile("ddsWrapper.dds.dll", "dds2.dll");
         }
     }
 
@@ -351,9 +351,9 @@ namespace DDS
     {
         public static void ExtractResourceToFile(string resourceName, string filename)
         {
-            if (!System.IO.File.Exists(filename))
-                using (System.IO.Stream s = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
-                using (System.IO.FileStream fs = new System.IO.FileStream(filename, System.IO.FileMode.Create))
+            if (!File.Exists(filename))
+                using (Stream s = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName)!)
+                using (FileStream fs = new FileStream(filename, FileMode.Create))
                 {
                     byte[] b = new byte[s.Length];
                     s.Read(b, 0, b.Length);
